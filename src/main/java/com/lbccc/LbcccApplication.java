27a274c23ad5d2aa71aadca6af06e0327a9a1190
Sam Andreev <82375774+Sifer-crack@users.noreply.github.com>
2024-09-23 19:35:51 +1200

    Application starting class

---
+++ src/main/java/com/lbccc/LbcccApplication.java
@@ -0,0 +1,13 @@
+package com.lbccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbcccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbcccApplication.class, args);
+    }
+
+}
